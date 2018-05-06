d125f6d833ab1f71f7364cd4aa8d556544c7b10c
petersommerhoff <petersommerhoff@gmail.com>
2018-05-06 19:07:28 +0200

    Add time estimates

--- oo/src/main/kotlin/_03_lab/exercise01.kt
+++ oo/src/main/kotlin/_03_lab/exercise01.kt
@@ -4,7 +4,7 @@ package _03_lab
  * @author Peter Sommerhoff <peter@petersommerhoff.com>
  */
 
-// Exercise: create a class IntVector with the following constructors:
+// Exercise: create a class IntVector with the following constructors: (3 min)
 // 1) one that accepts a collection of integers
 // 2) one that accepts a vararg of integers
 // 3) one without parameters (initializes an empty IntVector)
--- oo/src/main/kotlin/_03_lab/exercise02.kt
+++ oo/src/main/kotlin/_03_lab/exercise02.kt
@@ -4,7 +4,7 @@ package _03_lab
  * @author Peter Sommerhoff <peter@petersommerhoff.com>
  */
 
-// Exercise: create a class Student with the following members:
+// Exercise: create a class Student with the following members: (5-8 min)
 // 1) a name with a custom getter returning "Hey, my name's <NAME>" and a custom setter that rejects names without a space (should be a full name)
 // 2) a lazy property `homework` (let's just make it a String) that performs heavy crunching (Thread.sleep is okay) to generate a result, but only if necessary!
 // 3) an observable property outfit (again, simply a String for now) that logs when changes are made
@@ -12,5 +12,5 @@ package _03_lab
 // 4) methods to dressNormally(), dressFormally(), and dressAwfully() that change the outfit to an appropriate value
 // Insert code here...
 
-// Exercise: quick-check your implementation in a simple main() function
+// Exercise: quick-check your implementation in a simple main() function (2 min)
 // Insert code here...
